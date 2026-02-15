959f4499495d20a2cd3463d8edba29d95f585e9c
Stefan <git@stefn.co.uk>
2026-02-15 02:18:06 +0000

    Remove unused pages

--- walks/walks.11tydata.js
+++
@@ -1,6 +0,0 @@
-import { computeGallery } from "#collections/products.js";
-import { linkableContent } from "#utils/linkable-content.js";
-
-export default linkableContent("walks", {
-	gallery: computeGallery,
-});
